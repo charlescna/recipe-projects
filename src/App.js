e475bafc7a6744efcf218db90bca76a9ec2d6ef2
charlescna <122284631+charlescna@users.noreply.github.com>
2023-09-21 20:35:26 -0600

    testing useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  let recipeData = [ {name: "Pizza", Ingredients: "salt"}, {name: "pasta", ingredients: "tomatosauace" }]
+
+  const [recipes, setRecipes] = useState(null);
+
+  useEffect( () => {
+    setRecipes(recipeData)
+  }, [])
+
+
   return (
+    <div>
     <h1>Cooking recipes</h1>
+    {
+      recipes.map( (recipes, i) => {
+        return <h2>{recipes.name}</h2>
+      })
+    }
+  </div>
+
   );
 }
 
